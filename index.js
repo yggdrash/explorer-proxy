4d94d11453d1be775a35fd44bec3ecce1a7cd691
Luke Hwang <luke@r2v.io>
2019-03-21 16:19:57 +0900

    Add get txs api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { Block } = require('./qeury')
+const { Block, TxQeury } = require('./qeury')
 
 const PORT = 3000
 
@@ -13,6 +13,13 @@ app.get('/blocks', async (req, res) => {
     let size = req.query.size || 20
     let blocks = await Block.findAll(from, size)
     res.send(blocks)
+})
+
+app.get('/txs', async (req, res) => {
+    let from = req.query.from || 0
+    let size = req.query.size || 20
+    let txs = await TxQeury.findAll(from, size)
+    res.send(txs)
 })
 
 app.listen(PORT, () => {
