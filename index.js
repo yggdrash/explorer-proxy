abed7b3fcee4d1a4380345ae3613d4bfa5ba22e2
Luke Hwang <luke@r2v.io>
2019-03-21 16:16:30 +0900

    Add get blocks api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const { Block } = require('./qeury')
+
+const PORT = 3000
 
 app.get('/', (req, res) => {
     res.send('Welcome to YGGDRASH!')
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.get('/blocks', async (req, res) => {
+    let from = req.query.from || 0
+    let size = req.query.size || 20
+    let blocks = await Block.findAll(from, size)
+    res.send(blocks)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
